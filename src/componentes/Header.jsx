2c1cc0d17389644f694df07951f2a14012882d46
Erick Fujita <erickfujitao@gmail.com>
2024-08-15 15:36:46 -0300

    f

---
+++ src/componentes/Header.jsx
@@ -0,0 +1,26 @@
+function Header() {
+  return ( 
+  <>
+    <div className="container">
+    
+      <header id='header'className="nav-bar">
+        <div className="logo">
+        </div>
+      <h1 className="title">
+        Market Mobi
+      </h1>
+      <div className="contato">
+      <div className='instagram'>
+        <a href="https://www.instagram.com/marketmobi/" target="_blank">Instagram</a>
+      </div>
+      <div className='whatsapp'>
+        <a href="#" target="_blank">WhatsApp</a>
+      </div>
+      </div>
+      </header>
+    </div>
+  </>
+   );
+}
+
+export default Header;
