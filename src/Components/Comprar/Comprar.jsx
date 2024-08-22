f6081e3c6003894153c741ea4b1022a4c2ead15d
Erick Fujita <erickfujitao@gmail.com>
2024-08-22 14:47:11 -0300

    f

--- src/Components/Comprar/Comprar.jsx
+++ src/Components/Comprar/Comprar.jsx
@@ -2,7 +2,7 @@ import './Comprar.css'
 
 function Comprar() {
   return (
-    <div className="container-compra">
+    <div className="container-pagecompra">
       <div className="botoes-interativos">
         <div className="botao comprar">Comprar</div>
         <div className="botao regiiao">Escolher a região</div>
@@ -10,37 +10,66 @@ function Comprar() {
         <div className="botao valor">Valor</div>
       </div>
       <br />
-      <div className="box-imoveis">
-        <div className="img">
-          <img src="https://betaimages.lopes.com.br/realestate/REO5611/DFC685568C92234145E307AD63DC489B.JPG"/>
+      <div className="container-box">
+        <div className="box-imoveis">
+          <div className="img">
+            <img src="https://betaimages.lopes.com.br/realestate/REO5611/DFC685568C92234145E307AD63DC489B.JPG" />
+          </div>
+          <div className="box-texto">
+            <div className="infos-imovel-compra">
+              <h2>Imóvel 1</h2>
+            </div>
+            <div className="valor-imovel-compra">
+              <p>R$2.000.000</p>
+            </div>
+            <div className="conhecer-imovel-compra"></div>
+          </div>
         </div>
-        <div className="box-texto">
-        <div className="infos-imovel-compra">
-          <h2>Imóvel 1</h2>
+        <div className="box-imoveis">
+          <div className="img">
+            <img src="https://betaimages.lopes.com.br/realestate/REO5611/DFC685568C92234145E307AD63DC489B.JPG" />
+          </div>
+          <div className="box-texto">
+            <div className="infos-imovel-compra">
+              <h2>Imóvel 2</h2>
+            </div>
+            <div className="valor-imovel-compra">
+              <p>R$2.000.000</p>
+            </div>
+            <div className="conhecer-imovel-compra"></div>
+          </div>
         </div>
-        <div className="valor-imovel-compra">
-          <p>R$2.000.000</p>
+        <div className="box-imoveis">
+          <div className="img">
+            <img src="https://betaimages.lopes.com.br/realestate/REO5611/DFC685568C92234145E307AD63DC489B.JPG" />
+          </div>
+          <div className="box-texto">
+            <div className="infos-imovel-compra">
+              <h2>Imóvel 2</h2>
+            </div>
+            <div className="valor-imovel-compra">
+              <p>R$2.000.000</p>
+            </div>
+            <div className="conhecer-imovel-compra"></div>
+          </div>
         </div>
-        <div className="conhecer-imovel-compra"></div>
+        <div className="box-imoveis">
+          <div className="img">
+            <img src="https://betaimages.lopes.com.br/realestate/REO5611/DFC685568C92234145E307AD63DC489B.JPG" />
+          </div>
+          <div className="box-texto">
+            <div className="infos-imovel-compra">
+              <h2>Imóvel 2</h2>
+            </div>
+            <div className="valor-imovel-compra">
+              <p>R$2.000.000</p>
+            </div>
+            <div className="conhecer-imovel-compra"></div>
+          </div>
         </div>
       </div>
-
-      <div className="box-imoveis">
-        <div className="img">
-          <img src="https://betaimages.lopes.com.br/realestate/REO5611/DFC685568C92234145E307AD63DC489B.JPG"/>
-        </div>
-        <div className="box-texto">
-        <div className="infos-imovel-compra">
-          <h2>Imóvel 1</h2>
-        </div>
-        <div className="valor-imovel-compra">
-          <p>R$2.000.000</p>
-        </div>
-        <div className="conhecer-imovel-compra"></div>
-        </div>
       </div>
-    </div>
-  );
+      );
 }
 
-export default Comprar
+      export default Comprar
